fa209081384495cc36ce4e39afd0b5054bbafe65
Carsten Bleicker <carsten@bleicker.de>
2015-05-15 20:48:04 +0200

    Register addinional Locales

--- src/Configuration/Includes/Locales.php
+++ src/Configuration/Includes/Locales.php
@@ -3,3 +3,6 @@
 use Bleicker\Translation\Locale;
 
 Locale::register('English', 'en', 'GB');
+Locale::register('German', 'de', 'DE');
+Locale::register('Italian', 'de', 'DE');
+Locale::register('Japanese', 'ja', 'JP');
