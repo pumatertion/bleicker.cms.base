45f34ab092d9a27a570f253e35cb91fb54a68a6f
Carsten Bleicker <carsten@bleicker.de>
2015-05-16 09:32:25 +0200

    Fix italian locale

--- src/Configuration/Includes/Locales.php
+++ src/Configuration/Includes/Locales.php
@@ -4,5 +4,5 @@ use Bleicker\Translation\Locale;
 
 Locale::register('English', 'en', 'GB');
 Locale::register('German', 'de', 'DE');
-Locale::register('Italian', 'de', 'DE');
+Locale::register('Italian', 'it', 'IT');
 Locale::register('Japanese', 'ja', 'JP');
