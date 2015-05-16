7b4c8824f89066c6ef6e1fde5233525952f8a389
Carsten Bleicker <carsten@bleicker.de>
2015-05-16 09:42:43 +0200

    Register Modules

--- src/Configuration/Before.php
+++ src/Configuration/Before.php
@@ -6,4 +6,5 @@ include __DIR__ . '/Includes/Cache.php';
 include __DIR__ . '/Includes/Persistence.php';
 include __DIR__ . '/Includes/TypeConverter.php';
 include __DIR__ . '/Includes/NodeTypes.php';
+include __DIR__ . '/Includes/Modules.php';
 include __DIR__ . '/Includes/Security.php';
