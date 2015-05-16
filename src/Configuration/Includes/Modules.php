e3aed5344304395b70f881c4f2b344112e169d9d
Carsten Bleicker <carsten@bleicker.de>
2015-05-16 12:37:56 +0200

    Remove SetupController from Modules

--- src/Configuration/Includes/Modules.php
+++ src/Configuration/Includes/Modules.php
@@ -1,11 +1,7 @@
 <?php
 
 use Bleicker\Cms\Controller\NodeController;
-use Bleicker\Cms\Controller\SetupController;
 use Bleicker\Cms\Modules\ModuleInterface;
 
 NodeController::register('Node Manager', 'A node controller', ModuleInterface::MANAGEMENT_GROUP, '/nodemanager')
 	->addAllowedRoleName('Administrator')->addAllowedRoleName('Editor');
-
-SetupController::register('Setup', 'Setup Controller', ModuleInterface::MANAGEMENT_GROUP, '/setup')
-	->addAllowedRoleName('Administrator');
