104509860a856d721bde3adf73711eb928a3ca15
Carsten Bleicker <carsten@bleicker.de>
2015-05-18 15:01:29 +0200

    Fix Context

--- src/Configuration/Includes/Cache.php
+++ src/Configuration/Includes/Cache.php
@@ -1,6 +1,6 @@
 <?php
 
-use Bleicker\Framework\Context\Context;
+use Bleicker\Context\Context;
 use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Registry\Registry;
 use Doctrine\Common\Cache\Cache;
@@ -11,7 +11,7 @@ use TYPO3\Fluid\Core\Cache\SimpleFileCache;
 /**
  * Cache Configurations
  */
-if (Context::isProduction()) {
+if (Context::get(Context::APPLICATION_CONTEXT) === Context::PRODUCTION) {
 	ObjectManager::add(FluidCacheInterface::class, function () {
 		$fluidCache = new SimpleFileCache(Registry::get('paths.cache.default') . '/typo3.fluid');
 		ObjectManager::add(FluidCacheInterface::class, $fluidCache, TRUE);
--- src/Configuration/Includes/Persistence.php
+++ src/Configuration/Includes/Persistence.php
@@ -1,6 +1,6 @@
 <?php
 
-use Bleicker\Framework\Context\Context;
+use Bleicker\Context\Context;
 use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Persistence\EntityManager;
 use Bleicker\Persistence\EntityManagerInterface;
@@ -11,7 +11,8 @@ use Doctrine\ORM\Tools\Setup;
 ObjectManager::add(EntityManagerInterface::class, function () {
 	$entityManager = EntityManager::create(
 		Registry::get('doctrine.db.default'),
-		Setup::createYAMLMetadataConfiguration(Registry::get('paths.doctrine.schema'), !Context::isProduction(), Registry::get('paths.cache.default') . '/doctrine', ObjectManager::get(Cache::class))
+		Setup::createYAMLMetadataConfiguration(Registry::get('paths.doctrine.schema'),
+			Context::get(Context::APPLICATION_CONTEXT) !== Context::PRODUCTION, Registry::get('paths.cache.default') . '/doctrine', ObjectManager::get(Cache::class))
 	);
 	ObjectManager::add(EntityManagerInterface::class, $entityManager, TRUE);
 	return $entityManager;
