b526d310e55eee741abfeadb49cd6c4a960dd00f
Carsten Bleicker <carsten@bleicker.de>
2015-05-21 16:22:46 +0200

    Shared Registry

--- src/Configuration/Frontend/Before.php
+++ src/Configuration/Frontend/Before.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . '/Registry.php';
+include __DIR__ . '/../Shared/Registry.php';
 include __DIR__ . '/../Shared/Locales.php';
 include __DIR__ . '/../Shared/Cache.php';
 include __DIR__ . '/../Shared/Persistence.php';
