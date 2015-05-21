17cf97890179ccd7f96b408c8a423feacbff9ca0
Carsten Bleicker <carsten@bleicker.de>
2015-05-21 15:32:32 +0200

    Move before configurations to after configurations

--- src/Configuration/Backend/After.php
+++ src/Configuration/Backend/After.php
@@ -1,3 +1,7 @@
 <?php
 
 include __DIR__ . '/Routes.php';
+include __DIR__ . '/TypeConverter.php';
+include __DIR__ . '/NodeTypes.php';
+include __DIR__ . '/Modules.php';
+include __DIR__ . '/Security.php';
--- src/Configuration/Backend/Before.php
+++ src/Configuration/Backend/Before.php
@@ -4,7 +4,3 @@ include __DIR__ . '/Registry.php';
 include __DIR__ . '/../Shared/Locales.php';
 include __DIR__ . '/../Shared/Cache.php';
 include __DIR__ . '/../Shared/Persistence.php';
-include __DIR__ . '/TypeConverter.php';
-include __DIR__ . '/NodeTypes.php';
-include __DIR__ . '/Modules.php';
-include __DIR__ . '/Security.php';
