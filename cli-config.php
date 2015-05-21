662ed084d8e6df70ae436d17807fe595cf6ab227
Carsten Bleicker <carsten@bleicker.de>
2015-05-21 15:34:35 +0200

    Fix doctrine cli config

--- cli-config.php
+++ cli-config.php
@@ -4,8 +4,8 @@ use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Persistence\EntityManagerInterface;
 
 include __DIR__ . '/vendor/autoload.php';
-include __DIR__ . '/src/Configuration/Includes/Registry.php';
-include __DIR__ . '/src/Configuration/Includes/Cache.php';
-include __DIR__ . '/src/Configuration/Includes/Persistence.php';
+include __DIR__ . '/src/Configuration/Backend/Registry.php';
+include __DIR__ . '/src/Configuration/Shared/Cache.php';
+include __DIR__ . '/src/Configuration/Shared/Persistence.php';
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(ObjectManager::get(EntityManagerInterface::class));
